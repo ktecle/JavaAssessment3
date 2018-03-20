694a19e7abf7e0bc0c2cbf9ed39ceadb9c20d12b
Kibret Tecle <kibrettecle@zipcoders-MacBook-Pro-5.local>
2018-03-20 08:23:14 -0400

    making changes

--- src/main/java/user_management/UserCollection.java
+++ src/main/java/user_management/UserCollection.java
@@ -30,33 +30,49 @@ public class UserCollection extends ArrayList<User>{
     }
 
     public User attemptLogin(String email, String password) throws UserAuthenticationFailedException{
-        for(User user: this){
-            if(user.getEmail().equals(email)&& user.getPassword().equals(password)&&
-                    (user.getPassword().hashCode()==password.hashCode())){
-                return user;
+        User user = findByEmail(email);
+
+            if(user.getEmail()==null || !(user.getPassword().matches(password))){
+                throw new UserAuthenticationFailedException();
             }
-        }
-        return null;
+
+        return user;
+    }
+    Pattern validEmailPattern = Pattern.compile("([\\w\\.-]+@[\\w\\.-]+\\.[\\w]+)");
+    public boolean verifyEmailValidity(String email){
+        Matcher regexMatcher = validEmailPattern.matcher(email);
+        boolean validity= regexMatcher.find();
+        return validity;
+    }
+    Pattern validPasswordPattern = Pattern.compile("(?<=([Pp][Aa][Ss][Ww][Oo][Rr][Dd][^A-Za-z])).*?(?=[^A-Za-z0])");
+    public boolean verifyPasswordValidity(String password){
+        Matcher regexMatcher = validPasswordPattern.matcher(password);
+        boolean validity = regexMatcher.find();
+        return validity;
     }
 
     public int createUser(String name, String email, String password) throws EmailNotAvailableException,
                             InvalidEmailException,PasswordTooSimpleException {
         User newUser;
-        for(User user : this){
-            if(user.getEmail().equals(email)){
-                throw new EmailNotAvailableException();
-            }else if((email.contains("!#$%^&*()<>?"))||!isSubstring(email,name)) {
-                throw new InvalidEmailException();
-            }else{
-                int newId = this.size()+1;
-               newUser  = new User(newId,name,email,password);
-                this.add(newUser);
-                return newUser.getId();
-            }
+        int startId =1;
+        newUser =findById(startId);
+        if(newUser!=null){
+            newUser = new User(startId,name,email,password);
+            startId++;
         }
+
+
+        if(!verifyEmailValidity(email)){
+            throw new InvalidEmailException();
+        }else if (!verifyPasswordValidity(password)){
+            throw new PasswordTooSimpleException();
+        }
+
+
         return 0;
 
     }
+
     public boolean isSubstring(String str1,String str2){
         return str1.indexOf(str2)>=0;
     }
@@ -68,33 +84,14 @@ public class UserCollection extends ArrayList<User>{
         String name = regexMatcher.group();
         return name;
     }
-    public String findEmail(String user){
-            Pattern namePattern = Pattern.compile("([\\w\\.-]+@[\\w\\.-]+\\.[\\w]+)");
-            Matcher regexMatcher = namePattern.matcher(user);
-            String email = regexMatcher.group();
-            return email;
-    }
+
     public Integer findId(String user) {
         Pattern namePattern = Pattern.compile("(?<=([Ii][Dd])).*?(?=[^A-Za-z0])");
         Matcher regexMatcher = namePattern.matcher(user);
         Integer id = Integer.parseInt(regexMatcher.group());
         return id;
     }
-    public String findPassword(String user){
-        Pattern namePattern = Pattern.compile("(?<=([Pp][Aa][Ss][Ww][Oo][Rr][Dd][^A-Za-z])).*?(?=[^A-Za-z0])");
-        Matcher regexMatcher = namePattern.matcher(user);
-        String password = regexMatcher.group();
-        return password;
-    }
 
-    public User parseStringIntoUser(String user){
-        String name = findName(user);
-        String email = findEmail(user);
-        String password = findPassword(user);
-        Integer id = findId(user);
-
-        return new User(id,name,email,password);
-    }
 
 
 }
