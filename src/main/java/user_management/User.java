9e3753b8828d44493a82fb7d12fb1aafdc91c624
Kibret Tecle <kibrettecle@zipcoders-MacBook-Pro-5.local>
2018-03-19 20:54:45 -0400

    Fixed some of the errors

--- src/main/java/user_management/User.java
+++ src/main/java/user_management/User.java
@@ -14,6 +14,13 @@ public class User {
         this.email = email;
         this.password = new Password(password);
     }
+    public User(int id,String name,String email,Password password){
+        this.id =id;
+        this.name = name;
+        this.email =email;
+        this.password = password;
+
+    }
 
     @Override
     public String toString() {
@@ -32,8 +39,11 @@ public class User {
     public void setEmail(String email){
         this.email = email;
     }
+    public void setPassword(String password){
+        this.password = new Password(password);
+    }
     public void setPassword(Password password){
-        this.password = password;
+        this.password =password;
     }
     public void setId(int id){
         this.id = id;
--- src/main/java/user_management/UserCollectionInitializer.java
+++ src/main/java/user_management/UserCollectionInitializer.java
@@ -1,7 +1,19 @@
 package user_management;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.stream.JsonReader;
+import com.oracle.javafx.jmx.json.JSONReader;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
 public class UserCollectionInitializer {
-    public static UserCollection generate() {
-        return null;
+    public static UserCollection generate() throws FileNotFoundException{
+
+        JSONReader reader = (JSONReader) new JsonReader(new FileReader("users.json"));
+        Gson gson = new Gson();
+        UserCollection users= gson.fromJson((JsonElement) reader,UserCollection.class);
+        return users;
     }
 }
